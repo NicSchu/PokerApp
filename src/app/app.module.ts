574d7e4bfdf7139afdda78fb130ae20e0529d6c4
Nico <nicolai.schulz@mni.thm.de>
2017-06-15 12:28:33 +0200

    try to use firebase in login (not working yet)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import {ErrorHandler, NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {IonicApp, IonicErrorHandler, IonicModule} from "ionic-angular";
 import {MyApp} from "./app.component";
+import {AngularFireModule} from "angularfire2";
 
 import {TabsPage} from "./tabs/tabs";
 
+import {AngularFireDatabase, AngularFireDatabaseModule} from "angularfire2/database";
 import {StatusBar} from "@ionic-native/status-bar";
 import {SplashScreen} from "@ionic-native/splash-screen";
 import {SettingsPageComponent} from "./settings/settings-page.component";
@@ -12,6 +14,15 @@ import {ProfilePageComponent} from "./profile/profile-page.component";
 import {LobbyPageComponent} from "./lobby/lobby-page.component";
 import {FriendsPageComponent} from "./friends/friends-page.component";
 import {LoginPageComponent} from "./login/login-page.component";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCi2KvgtcoIz_DqETuUE3d9G8-_GCNunmw",
+  authDomain: "poker-b817e.firebaseapp.com",
+  databaseURL: "https://poker-b817e.firebaseio.com/",
+  projectId: "poker-b817e",
+  storageBucket: "poker-b817e.appspot.com",
+  messagingSenderId: "916592085687"
+};
 
 @NgModule({
   declarations: [
@@ -25,7 +36,9 @@ import {LoginPageComponent} from "./login/login-page.component";
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -40,7 +53,8 @@ import {LoginPageComponent} from "./login/login-page.component";
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AngularFireDatabase
   ]
 })
 export class AppModule {}
