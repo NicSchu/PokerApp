34031f97a83b5d733633d2257d06b7c6b70f18c9
Nico <nicolai.schulz@mni.thm.de>
2017-06-15 10:57:43 +0200

    added: login-page.component.html login-page.component.ts

---
+++ src/app/login/login-page.component.ts
@@ -0,0 +1,17 @@
+import {Component} from "@angular/core";
+import {Login} from './login.model'
+
+@Component({
+  selector:'profile-page',
+  templateUrl: 'login-page.component.html'
+})
+export class LoginPageComponent{
+
+  signIn(){
+    //TODO: hier firebasezeug
+  }
+
+  createAccount(){
+    //TODO: hier neue page aufrufen
+  }
+}
