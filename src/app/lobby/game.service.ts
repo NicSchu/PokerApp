8d367a8cef7826c7467b667fd14424e0957a30a3
Silas Schwabe <jan.hendrik.silas.schwabe@mni.thm.de>
2017-07-05 12:46:27 +0200

    bla

--- src/app/lobby/game.service.ts
+++ src/app/lobby/game.service.ts
@@ -1,30 +1,30 @@
-
-
 import {Player} from "./player.model";
 import {PlayingCard} from "../logic/cards.model";
 import {Deck} from "./deck.model";
+import {Observable} from "rxjs/Observable";
 
 export class GameService {
   turnCounter: number = 0;
+  public deck: Observable<Deck>;
 
-  constructor(public deck: Deck = new Deck,
-              public pot : number = 0,
+  constructor(public pot : number = 0,
               public table: PlayingCard[] = new PlayingCard[5],
               public players : Player[] = []) {}
 
+
   initializeRound() {
-    this.deck.shuffle();
+    //this.deck.shuffle();
 
     for (let i = 0; i < this.players.length; i++) {
-      this.players[i].hand[1] = this.deck.card.pop();
-      this.players[i].hand[2] = this.deck.card.pop();
+      //this.players[i].hand[1] = this.deck.card.pop();
+      //this.players[i].hand[2] = this.deck.card.pop();
     }
     for (let i = 0; i < 5; i++) {
-      this.table[i] = this.deck.card.pop();
+      //this.table[i] = this.deck.card.pop();
     }
   }
 
   reset() {
-    this.deck.reCreateDeck();
+    //this.deck.reCreateDeck();
   }
 }
