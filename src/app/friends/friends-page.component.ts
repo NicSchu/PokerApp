e71b7a42a55c1ea1f44f93d5f65c008355835982
Sebastian Bäcker <seb.b92@web.de>
2017-07-07 21:03:02 +0200

    Friend-List works (SEMI)

--- src/app/friends/friends-page.component.ts
+++ src/app/friends/friends-page.component.ts
@@ -3,9 +3,11 @@ import {ProfileService} from "../profile/profile.service";
 import {Profile} from "../profile/profile.model";
 import {AuthService} from "../login/AuthService";
 import {Observable} from "rxjs";
-import {NavController, NavParams} from "ionic-angular";
+import {ModalController, NavController, NavParams} from "ionic-angular";
 import {FriendsAddPageComponent} from "./friends-add-page.component";
 import {LocalStorageService} from "../common/local-storage.service";
+import {FriendsService} from "./friends.service";
+import {SubscriptionService} from "../tabs/subscription.service";
 /**
  * Created by sebb9 on 08.06.2017.
  */
@@ -18,13 +20,18 @@ import {LocalStorageService} from "../common/local-storage.service";
 export class FriendsPageComponent {
 
   profile : Profile;
-  friendProfiles : Observable<Profile>[] = [];
-  filteredFriendProfiles : Profile[] = [];
+
+  private allProfiles: Profile[];
+  private friendProfiles: Profile[] = [];
+  private filteredFriendProfiles: Profile[] = [];
 
   constructor(private profileService : ProfileService,
-              private authService : AuthService,
               private navCtrl: NavController,
+              private authService: AuthService,
+              private modalCtrl: ModalController,
+              private subscriptionService: SubscriptionService,
               private navParams: NavParams,
+              private friendsService: FriendsService,
               private localStorageService: LocalStorageService) { //localStorageService is used in the HTML file (<ion-navbar> tag)
 
     if (this.navParams.data) {
@@ -34,15 +41,42 @@ export class FriendsPageComponent {
   }
 
   ionViewDidLoad() {
+    this.subscriptionService.addSubscription(
+      this.friendsService.findAll().subscribe(
+        (allProfiles: Profile[]) => {
+          //Ich hoffe er bekommt hier mit, wenn sich am eigentlichen "User"-Objekt was ändert, evtl notwendig für Freunde von Freunden!!!!
+          this.allProfiles = allProfiles;
 
+          //now filter friends of User
+          for (let i = 0; i < this.profile.friends.length; i++) {
+            for (let j = 0; j < this.allProfiles.length; j++) {
+              let foundFriendProfile: Profile;
+              if (this.profile.friends[i] === this.allProfiles[j].email) {
+                foundFriendProfile = this.allProfiles[j];
+                this.friendProfiles.push(foundFriendProfile);
+              }
+            }
+          }
+
+          //set filtered to all....
+          //TODO - maybe sort Friends by Name
+          this.filteredFriendProfiles = this.friendProfiles;
+        }
+      )
+    );
 
   }
 
   public showAddFriend() : void {
+    //TODO - ich denke das hier muss mit einem Modal gemacht werden, damit wir im onDidMiss die Friendlist aktualisieren können!
     this.navCtrl.push(FriendsAddPageComponent, this.profile);
   }
 
   public showFriendLeaderboard() : void {
     //TODO - implement
+  }
+
+  public showFriendProfilePage(profile: Profile): void {
+    //TODO - implement!!!!
   }
 }
